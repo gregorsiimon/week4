a2263d103b3b5230f84f6dc79a09837e7c95e445
Gregor Siimon <gregor.siimon@edu.murg.ee>
2024-01-18 16:51:09 +0200

    Week 4

---
+++ script.js
@@ -0,0 +1,9 @@
+const randomNumberFeedback = document.querySelector("#randomNumber");
+const generateRandomBtn = document.querySelector("#generateRandomBtn");
+
+generateRandomBtn.addEventListener("click", () => {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    const feedbackMessage = `Random Number: ${randomNumber}`;
+    console.log(feedbackMessage);
+    randomNumberFeedback.innerHTML = feedbackMessage;
+});
